be1e2f0e74741855fc2d2a04b2ffeb8c5686e0be
Annija Maslovska <annija.maslovska6052@gmail.com>
2026-06-15 18:39:20 +0300

    Pievienotas modelu attiecibas

--- app/Models/Equipment.php
+++ app/Models/Equipment.php
@@ -3,8 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Equipment extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'status',
+        'is_public',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
 }
--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Reservation extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'equipment_id',
+        'start_date',
+        'end_date',
+        'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function equipment()
+    {
+        return $this->belongsTo(Equipment::class);
+    }
 }
